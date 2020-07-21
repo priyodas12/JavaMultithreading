7ed7ff273f4a20539b4a03a2f91aa4b96b936ef0
Priyobrato Das <priyodas12@gmail.com>
2020-07-21 20:58:12 +0530

    single thread

---
+++ src/io/ExecutorService/SingleTPExecution.java
@@ -0,0 +1,21 @@
+package io.ExecutorService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SingleTPExecution {
+
+    public static void main(String[] args) {
+        //task will be placed in blocking queue
+        //executable thread size is one ,best for predefined sequential task.
+        ExecutorService executorService= Executors.newSingleThreadExecutor();
+
+        for(int i=0;i<100;i++){
+            executorService.execute(new Task());
+        }
+
+        System.out.println("Thread Name:"+Thread.currentThread().getName());
+
+        executorService.shutdown();
+    }
+}
