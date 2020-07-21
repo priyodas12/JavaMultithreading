dd80a5986ea695cbbb3318fd069f6858ec1aec4c
Priyobrato Das <priyodas12@gmail.com>
2020-07-21 20:54:25 +0530

    scheduled TP

--- src/io/ExecutorService/ScheduledTPExecution.java
+++ src/io/ExecutorService/ScheduledTPExecution.java
@@ -1,4 +1,24 @@
 package io.ExecutorService;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class ScheduledTPExecution {
+    public static void main(String[] args) {
+        //task will be stored in delay queue with the sequence of less delay object in priority position.
+        ScheduledExecutorService executorService= Executors.newScheduledThreadPool(10);
+
+        //executorService.schedule(new Task(),10, TimeUnit.SECONDS);
+
+        for(int i=0;i<100;i++){
+            //task to tun after 10 sec delay.
+            executorService.schedule(new Task(),10, TimeUnit.SECONDS);
+        }
+
+        System.out.println("Thread Name:"+Thread.currentThread().getName());
+
+        executorService.shutdown();
+    }
 }
